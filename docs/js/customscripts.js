b8206f1db542c0fafd100c48c7fe7cb5d3ae9c73
Taylor Rockey <tarockey@microsoft.com>
2018-03-08 11:35:20 -0800

    Updated config, opening all external links in new tab

--- docs/js/customscripts.js
+++ docs/js/customscripts.js
@@ -52,3 +52,20 @@ $(function() {
         }
     });
 });
+//Open external links in new tab
+$(function () {
+    handleLinks()
+})()
+
+function handleLinks () {
+    // Don't open our own site's links in new tabs
+    var host = location.hostname 
+    // Grab and loop over all <a> elements
+    var allLinks = document.querySelectorAll('a') 
+    for (var i = 0; i < allLinks.length; ++i) {
+        // if links are external and not empty
+        if (allLinks[i].hostname !== host && allLinks[i].hostname !== '') {
+        allLinks[i].target = '_blank'
+        }
+    }
+}
